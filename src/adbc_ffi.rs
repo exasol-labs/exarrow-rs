f55661852262e7aac5007b0ab93fb3b627cabd3d
David Li <li.davidm96@gmail.com>
2026-07-29 14:51:07 +0900

    fix: export AdbcDriverExasolInit
    This is the symbol that driver managers expect.

--- src/adbc_ffi.rs
+++ src/adbc_ffi.rs
@@ -2385,8 +2385,21 @@ impl adbc_core::Statement for FfiStatement {
 // -----------------------------------------------------------------------------
 
 // Export the driver using the adbc_ffi macro.
-// The exported function will be named `ExarrowDriverInit`.
-adbc_ffi::export_driver!(ExarrowDriverInit, FfiDriver);
+// The exported function will be named `AdbcDriverExasolInit`.
+adbc_ffi::export_driver!(AdbcDriverExasolInit, FfiDriver);
+
+#[allow(non_snake_case)]
+#[unsafe(no_mangle)]
+pub unsafe extern "C" fn ExarrowDriverInit(
+    version: std::os::raw::c_int,
+    driver: *mut std::os::raw::c_void,
+    error: *mut adbc_ffi::FFI_AdbcError,
+) -> adbc_core::error::AdbcStatusCode {
+    // Compatibility alias. AdbcDriverExasolInit is the "proper" name
+    // (note the lowercase) but initial versions exported ExarrowDriverInit
+    // instead.
+    AdbcDriverExasolInit(version, driver, error)
+}
 
 #[cfg(test)]
 mod tests {
